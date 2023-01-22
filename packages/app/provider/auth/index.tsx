e2ad644ae7cfab95a64f098c72b0c580c35b7368
Robert Taussig <robert.taussig@gmail.com>
2023-01-22 16:50:41 -0500

    Finish upgrade

--- packages/app/provider/auth/index.tsx
+++ packages/app/provider/auth/index.tsx
@@ -2,7 +2,7 @@ import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from './cache';
 
 //this should be the same as your next js frontend key
-const CLERK_PUBLISHABLE_KEY = 'clerk.logical.pegasus-13.lcl.dev'; //enter your clerk key here!
+const CLERK_PUBLISHABLE_KEY = 'pk_test_Z2xvcmlvdXMtZ29iYmxlci01Mi5jbGVyay5hY2NvdW50cy5kZXYk'; //enter your clerk key here!
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
